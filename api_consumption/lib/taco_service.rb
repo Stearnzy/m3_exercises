32ced1b9db29a3bbf4fdebe609512e2781265bb0
Zach Stearns <zach.stearns92@gmail.com>
2020-12-03 23:49:21 -0700

    Random_ingredients passing

--- api_consumption/lib/taco_service.rb
+++ api_consumption/lib/taco_service.rb
@@ -3,10 +3,15 @@ require 'json'
 
 class TacoService
   def conn
-    Faraday.new('http://taco-randomizer.herokuapp.com/')
+    Faraday.new('http://taco-randomizer.herokuapp.com')
   end
 
   def parse(response)
     JSON.parse(response)
+  end
+
+  def random_ingredients
+    response = conn.get('/random/')
+    parse(response.body)
   end
 end
