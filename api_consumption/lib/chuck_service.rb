fb641dd3db9c9f7b112cf7df46b873da43688429
Zach Stearns <zach.stearns92@gmail.com>
2020-12-03 23:15:22 -0700

    conn, parse and random methods created, tests passing

--- api_consumption/lib/chuck_service.rb
+++ api_consumption/lib/chuck_service.rb
@@ -2,5 +2,16 @@ require 'faraday'
 require 'json'
 
 class ChuckService
-  
+  def conn
+    Faraday.new('https://api.chucknorris.io')
+  end
+
+  def parse(response)
+    JSON.parse(response)
+  end
+
+  def random
+    response = conn.get('/jokes/random')
+    parse(response.body)
+  end
 end
