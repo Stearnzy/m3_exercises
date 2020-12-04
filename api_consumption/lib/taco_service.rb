a731dac1184cce62afe5239756689e43c58d9f21
Zach Stearns <zach.stearns92@gmail.com>
2020-12-03 23:27:17 -0700

    conn and parse methods made

--- api_consumption/lib/taco_service.rb
+++ api_consumption/lib/taco_service.rb
@@ -2,5 +2,11 @@ require 'faraday'
 require 'json'
 
 class TacoService
-  
+  def conn
+    Faraday.new('http://taco-randomizer.herokuapp.com/')
+  end
+
+  def parse(response)
+    JSON.parse(response)
+  end
 end
